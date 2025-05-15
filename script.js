ab3c01555ea1d006470b9258e2fc83cd6f9ee989
mobinkaseban <mobinkaseban@gmail.com>
2025-05-15 20:54:09 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleDetails(item) {
+  document.querySelectorAll('.menu-item').forEach(i => {
+    if (i !== item) i.classList.remove('active');
+  });
+  item.classList.toggle('active');
+}
