7ec953e5ecfae646e89a952aef53905fc403312c
mobinkaseban <mobinkaseban@gmail.com>
2025-05-16 13:33:19 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 function addToCart(item, price) {
-  cart.push({item, price});
+  cart.push({ item, price });
   localStorage.setItem('cart', JSON.stringify(cart));
-  alert("به سبد خرید افزوده شد!");
+  alert("افزوده شد!");
 }
 function loadCart() {
   const container = document.getElementById("cart-items");
@@ -11,23 +10,25 @@ function loadCart() {
   let sum = 0;
   container.innerHTML = "";
   cart.forEach((c, i) => {
-    container.innerHTML += `<div>${i + 1}. ${c.item} - ${c.price} تومان</div>`;
+    container.innerHTML += `<div>${i + 1}. ${c.item} - ${c.price.toLocaleString()} تومان</div>`;
     sum += c.price;
   });
-  total.innerText = "جمع کل: " + sum + " تومان";
+  total.innerText = "جمع کل: " + sum.toLocaleString() + " تومان";
 }
 function printInvoice() {
   window.print();
 }
 function sendToWhatsApp() {
-  let msg = "سفارش من:\n";
-  let total = 0;
+  let msg = "سفارش:
+", total = 0;
   cart.forEach(c => {
-    msg += `- ${c.item} (${c.price} تومان)\n`;
+    msg += `- ${c.item} (${c.price.toLocaleString()} تومان)
+`;
     total += c.price;
   });
-  msg += "\nجمع کل: " + total + " تومان";
-  window.open(`https://wa.me/989376899233?text=${encodeURIComponent(msg)}`, '_blank');
+  msg += `
+جمع کل: ${total.toLocaleString()} تومان`;
+  window.open(`https://wa.me/989376899233?text=${encodeURIComponent(msg)}`, "_blank");
 }
 window.onload = function() {
   if (document.getElementById("cart-items")) loadCart();
